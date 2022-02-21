0709bb7d65b4c110134a2ba7638c84636d64a631
myfahmi21 <fahmiahmadi123@gmail.com>
2022-02-21 15:13:11 +0700

    create component App.jsx, Search.jsx And User.jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import App from './components/App';
+import Search from './components/Search';
+import User from './components/User';
 
 function App() {
   return (
